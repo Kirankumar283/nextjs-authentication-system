a5367269ab88630d333d699aba68bfdeda9731a5
Kiran Prajapati <devkiranprajapati@gmail.com>
2026-02-11 18:41:31 +0530

    feat: initialize Next.js project with TypeScript and Tailwind CSS

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen py-4 justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-2xl text-neutral-800 dark:text-neutral-200 font-bold">
+        NextJS Authentication System
+      </h1>
+    </div>
+  );
+}
